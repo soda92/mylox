3c688e0afa8ef83872ce4ac2fadb90cc4152411a
SodaCris <wang-jiaxun@outlook.com>
2022-05-17 18:51:11 +0800

    add basic scanner

--- mylox/Lox.java
+++ mylox/Lox.java
@@ -1,9 +1,9 @@
-package mylox;
-
 import java.io.*;
 import java.nio.charset.*;
 import java.nio.file.*;
 import java.util.*;
+
+import static TokenType.*;
 
 public class Lox {
   public static void main(String[] args) throws IOException {
@@ -44,7 +44,7 @@ public class Lox {
     List<Token> tokens = scanner.scanTokens();
 
     for (Token token : tokens) {
-    System.out.println(token);
+      System.out.println(token);
     }
   }
 
@@ -59,4 +59,113 @@ public class Lox {
   }
 
   static boolean hadError = false;
+}
+
+enum TokenType {
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+  IDENTIFIER, STRING, NUMBER,
+
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE
+
+  , EOF
+}
+
+class Token {
+  final TokenType type;
+  final String lexeme;
+  final Object literal;
+  final int line;
+
+  Token(TokenType type, String lexeme, Object literal, int line) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  public String toString() {
+    return type + " " + lexeme + " " + literal;
+  }
+
+}
+
+class Scanner {
+  private final String source;
+  private final List<Token> tokens = new ArrayList<>();
+
+  Scanner(String source) {
+    this.source = source;
+  }
+
+  List<Token> scanTokens() {
+    while (!isAtEnd()) {
+      start = current;
+      scanToken();
+    }
+
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
+  }
+
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
+
+  private boolean isAtEnd() {
+    return current >= source.length();
+  }
+
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+      case '(':
+        addToken(LEFT_PAREN);
+        break;
+      case ')':
+        addToken(RIGHT_PAREN);
+        break;
+      case '{':
+        addToken(LEFT_BRACE);
+        break;
+      case '}':
+        addToken(RIGHT_BRACE);
+        break;
+      case ',':
+        addToken(COMMA);
+        break;
+      case '.':
+        addToken(DOT);
+        break;
+      case '-':
+        addToken(MINUS);
+        break;
+      case '+':
+        addToken(PLUS);
+        break;
+      case ';':
+        addToken(SEMICOLON);
+        break;
+      case '*':
+        addToken(STAR);
+        break;
+    }
+  }
+
+  private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
+  }
 }
