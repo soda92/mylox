5179e16bec08ab109168a8780fc763076e863095
SodaCris <soda@example.com>
2022-05-18 19:36:57 +0800

    use custom build

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
-mvn -q package
+javac lox.java
+if [ $? -eq 0 ]
+then
+  [ ! -d "lox" ] && mkdir lox
+  mv *.class lox
+  jar cfm lox.jar Manifest.txt lox/*.class
+else
+  exit $?
+fi
 
 if [ $? -eq 0 ]
 then
-  java -jar build/mylox-trunk.jar $@
+  java -jar lox.jar $@
 fi
