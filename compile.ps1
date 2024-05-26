e811134e80d5234f3cfb1f7ff51023e370014302
Maple <wang-jiaxun@outlook.com>
2024-05-26 19:38:34 +0800

    fix javac version

--- compile.ps1
+++ compile.ps1
@@ -2,7 +2,7 @@ Write-Host -ForegroundColor Green "generating..."
 py gen.py
 if($LastExitCode -ne 0){ exit $LastExitCode }
 Write-Host -ForegroundColor Green "compiling..."
-javac -g out.java
+javac --target 17 --source 17 -g out.java
 if($LastExitCode -ne 0){ exit $LastExitCode }
 if(!(Test-Path lox)){
   New-Item -ItemType Directory -Path lox | Out-Null
